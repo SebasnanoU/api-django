37d462eb505cf985f1fc83c805324ee1ef4494f3
sebasnanou <jsrodriguezp03@libertadores.edu.co>
2022-06-04 21:45:09 -0500

    prueba django 3

--- app/api/tests.py
+++ app/api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestApi(TestCase):
+    def test_api_root(self):
+        response = self.client.get('/api/task/')
+        self.assertEqual(response.status_code, 401)
